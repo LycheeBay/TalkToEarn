249dffcc79f92a8776a22cc997bf0cc341c5d2c5
You Song Hou <ysongweb3@gmail.com>
2025-08-16 22:56:00 -0400

    chore: add Flow EVM Testnet network to hardhat.config.js

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,6 +1,18 @@
+require('dotenv').config();
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    flowEVMTestnet: {
+      url: "https://testnet.evm.nodes.onflow.org",
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 545
+    },
+  },
+  paths: {
+    artifacts: '../react/src/artifacts',
+    cache: '../react/src/cache',
+  }
 };
